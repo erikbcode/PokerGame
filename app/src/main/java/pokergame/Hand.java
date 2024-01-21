3248521c2a14d20af1a3dd3954bf8eb73cea7cbc
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-21 17:11:04 -0600

    Added Hand class

---
+++ app/src/main/java/pokergame/Hand.java
@@ -0,0 +1,36 @@
+package pokergame;
+
+import java.util.List;
+
+import pokergame.exceptions.MisdealException;
+
+import java.util.ArrayList;
+
+public class Hand {
+  
+  private List<Card> hand;
+
+  public Hand() {
+    hand = new ArrayList<Card>();
+  }
+
+  public void clearHand() {
+    this.hand.clear();
+  }
+
+  public void addCard(Card card) {
+    if (hand.size() >= 2) {
+      throw new MisdealException("Player cannot have more than two cards");
+    }
+
+    this.hand.add(card);
+  }
+
+  public String toString() {
+    StringBuilder str = new StringBuilder();
+    str.append(this.hand.get(0).toString());
+    str.append(", ");
+    str.append(this.hand.get(1)).toString();
+    return str.toString();
+  }
+}
