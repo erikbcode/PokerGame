8ec23d38d0f1cebf23d6a7498cd12a3d742db2ba
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-21 17:13:33 -0600

    Added PokerGameRunner class to demonstrate a game

---
+++ app/src/main/java/pokergame/PokerGameRunner.java
@@ -0,0 +1,66 @@
+package pokergame;
+
+import java.util.Scanner;
+
+public class PokerGameRunner {
+  
+
+  public static void main (String[] args) {
+
+    Table table = new Table();
+    Dealer dealer = new Dealer("John");
+
+    table.setDealer(dealer);
+    dealer.sitAtTable(table);
+
+    AIPlayer aiPlayer1 = new AIPlayer();
+    AIPlayer aiPlayer2 = new AIPlayer();
+    AIPlayer aiPlayer3 = new AIPlayer();
+    AIPlayer aiPlayer4 = new AIPlayer();
+    AIPlayer aiPlayer5 = new AIPlayer();
+
+    table.addSeatedPlayer(aiPlayer2);
+    table.addSeatedPlayer(aiPlayer3);
+    table.addSeatedPlayer(aiPlayer4);
+    table.addSeatedPlayer(aiPlayer5);
+    table.addSeatedPlayer(aiPlayer1);
+
+    System.out.println("----- Welcome to the poker game -----");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("How much would you like to deposit ($): ");
+    int playerDeposit = scanner.nextInt();
+
+    HumanPlayer humanPlayer = new HumanPlayer(playerDeposit);
+    table.addSeatedPlayer(humanPlayer);
+    
+    for (int i = 0; i < 5; i++) {
+      System.out.println("----- NEW ROUND -----");
+      // Ask for bet
+      System.out.print("What should the bet be? ");
+      int bet = scanner.nextInt();
+
+      // Collect bets from players
+      dealer.collectBets(bet);
+
+      // deal player cards
+      dealer.dealHoleCards();
+
+      for (Player player : table.getActivePlayers()) {
+        System.out.println(player.getHand().toString());
+      }
+
+      // deal board cards
+      dealer.dealBoardCards();
+
+      // Check winner
+      System.out.println("Board is: " + table.getBoardCards());
+      System.out.println();
+      dealer.endRound();
+    }
+    
+
+    scanner.close();
+  }
+}
