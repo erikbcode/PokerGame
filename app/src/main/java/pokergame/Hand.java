358a469afec527f3d815f5901deead82e576f146
ERIK BUINEVICIUS <erikbcode@gmail.com>
2024-01-21 23:14:42 -0600

    Changed name of List<Card> variable to cards

--- app/src/main/java/pokergame/Hand.java
+++ app/src/main/java/pokergame/Hand.java
@@ -8,33 +8,33 @@ import java.util.ArrayList;
 
 public class Hand {
   
-  private List<Card> hand;
+  private List<Card> cards;
 
   public Hand() {
-    hand = new ArrayList<Card>();
+    cards = new ArrayList<Card>();
   }
 
   public void clearHand() {
-    this.hand.clear();
+    this.cards.clear();
   }
 
   public void addCard(Card card) {
-    if (hand.size() >= 2) {
+    if (cards.size() >= 2) {
       throw new MisdealException("Player cannot have more than two cards");
     }
 
-    this.hand.add(card);
+    this.cards.add(card);
   }
 
   public String toString() {
     StringBuilder str = new StringBuilder();
-    str.append(this.hand.get(0).toString());
+    str.append(this.cards.get(0).toString());
     str.append(", ");
-    str.append(this.hand.get(1)).toString();
+    str.append(this.cards.get(1)).toString();
     return str.toString();
   }
 
   public List<Card> getCards() {
-    return this.hand;
+    return this.cards;
   }
 }
